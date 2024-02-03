e939b33ca4d5f8f57a05217887ac14a7e16c4d7c
Dhyan Rai <dhyanraibm@gmail.com>
2024-02-04 01:02:14 +0530

    minor updates

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -46,13 +46,20 @@ const registerUser = asyncHandler(async (req, res, next) => {
     if (existedUser) throw new ApiError(400, "User already exists");
 
 
-    const avatarLocalPath = req.files?.avatar[0]?.path;
+    // const avatarLocalPath = req.files?.avatar[0]?.path;
     //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let avatarLocalPath;
+    if (req.files && Array.isArray(req.files.avatar) && req.files.avatar.length > 0) {
+        avatarLocalPath = req.files.avatar[0].path
+    }
 
     let coverImageLocalPath;
     if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
         coverImageLocalPath = req.files.coverImage[0].path
     }
+
+    console.log(avatarLocalPath, coverImageLocalPath);
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
